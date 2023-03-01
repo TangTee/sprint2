fb0980e4d0eabb408c63360d51097aeee3cc42db
ppppeace <98012373+ppppeace@users.noreply.github.com>
2023-03-01 15:11:13 +0700

    Update Login.dart
    แก้ key bord

--- lib/regis,login/Login.dart
+++ lib/regis,login/Login.dart
@@ -39,201 +39,205 @@ class _LoginState extends State<Login> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: lightPurple,
-      extendBodyBehindAppBar: false,
-      appBar: AppBar(
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back_ios, color: white),
-          onPressed: () {
-            Navigator.of(context).pop();
-          },
-        ),
-        toolbarHeight: 171,
-        flexibleSpace: Container(
-          decoration: const BoxDecoration(
-              borderRadius: BorderRadius.only(
-                  bottomLeft: Radius.circular(200),
-                  bottomRight: Radius.circular(200)),
-              image: DecorationImage(
-                  image: AssetImage("assets/images/login.png"),
-                  fit: BoxFit.fill)),
-        ),
-        backgroundColor: transparent,
-        elevation: 0,
-      ),
-      body: _isLoading
-          ? Center(
-              child: CircularProgressIndicator(
-                  color: Theme.of(context).primaryColor))
-          : SingleChildScrollView(
-              child: Form(
-                key: _formKey,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    const Text(
-                      "WELCOME BACK",
-                      style: TextStyle(
-                          fontSize: 46,
-                          fontWeight: FontWeight.bold,
-                          fontFamily: 'MyCustomFont',
-                          color: lightestGreen),
-                    ),
-                    const SizedBox(
-                      height: 05,
-                    ),
-                    const Text(
-                      "Login with TungTee account",
-                      style: TextStyle(
-                          fontSize: 14,
-                          fontWeight: FontWeight.w400,
-                          fontFamily: 'MyCustomFont',
-                          color: primaryColor),
-                    ),
-                    const SizedBox(
-                      height: 25,
-                    ),
-                    Container(
-                      alignment: Alignment.center,
-                      width: 360,
-                      child: TextFormField(
-                        controller: emailController,
-                        decoration: textInputDecoration.copyWith(
-                          hintText: 'Email  ',
-                          prefixIcon: const Icon(
-                            Icons.account_circle,
-                            color: green,
-                          ),
-                        ),
-                        onChanged: (val) {
-                          setState(() {
-                            email = val;
-                          });
-                        },
-                        validator: (val) {
-                          return RegExp(
-                                      r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
-                                  .hasMatch(val!)
-                              ? null
-                              : "Please enter a valid email";
-                        },
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 25,
-                    ),
-
-                    Container(
-                      alignment: Alignment.center,
-                      width: 360,
-                      child: TextFormField(
-                        controller: passwordController,
-                        obscureText: true,
-                        decoration: textInputDecoration.copyWith(
-                          hintText: 'Password',
-                          prefixIcon: const Icon(
-                            Icons.lock,
-                            color: green,
-                          ),
-                        ),
-                        onChanged: (val) {
-                          setState(() {
-                            password = val;
-                          });
-                        },
-                        validator: (val) {
-                          if (val!.length < 6) {
-                            return "Password must be at least 6 characters";
-                          } else {
-                            return null;
-                          }
-                        },
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 7,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        TextButton(
-                            style: TextButton.styleFrom(
-                              primary: primaryColor,
-                            ),
-                            onPressed: () {
-                              nextScreen(context, ResetPasswordPage());
-                            },
-                            child: const Text("Forget Password?"))
-                      ],
-                    ),
-                    const SizedBox(
-                      height: 50,
-                    ),
-                    //ElevatedButton.icon(
-                    // onPressed: () {
-                    //   login();
-                    // },
-                    // icon: const Icon(Icons.lock_open),
-                    // label: const Text('Login'),
-                    // ),
-                    ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                          backgroundColor: mobileBackgroundColor,
-                          side: const BorderSide(
-                            width: 2.0,
-                            color: purple,
-                          ),
-                          minimumSize: const Size(307, 49),
-                          elevation: 0,
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(30))),
-                      child: const Text(
-                        "Login",
-                        style: TextStyle(color: purple, fontSize: 24),
-                      ),
-                      onPressed: () {
-                        login();
-                      },
-                    ),
-
-                    //TextButton(
-                    // onPressed: () =>
-                    //    Navigator.pushNamed(context, '/register'),
-                    // child: const Text('ยังไม่มีบัญชีหรอ? สมัครใช้งาน'),
-                    // ),
-                    const SizedBox(
-                      height: 15,
-                    ),
-                    Text.rich(TextSpan(
-                      style: const TextStyle(
-                          color: mobileSearchColor,
-                          fontSize: 12,
-                          fontFamily: 'MyCustomFont'),
-                      children: <TextSpan>[
-                        TextSpan(
-                            text: "No account with TungTee? ",
-                            style: const TextStyle(
-                                color: primaryColor,
-                                decoration: TextDecoration.none),
-                            recognizer: TapGestureRecognizer()),
-                        TextSpan(
-                            text: "Register",
-                            style: const TextStyle(
-                                color: lightGreen,
-                                decoration: TextDecoration.underline,
-                                fontWeight: FontWeight.bold),
-                            recognizer: TapGestureRecognizer()
-                              ..onTap = () {
-                                nextScreen(context, const RegisterPage());
-                              }),
-                      ],
-                    )),
-                  ],
-                ),
-              ),
+    return MaterialApp(
+      home: DismissKeyboard(
+        child: Scaffold(
+          backgroundColor: lightPurple,
+          extendBodyBehindAppBar: false,
+          appBar: AppBar(
+            leading: IconButton(
+              icon: const Icon(Icons.arrow_back_ios, color: white),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
             ),
+            toolbarHeight: 171,
+            flexibleSpace: Container(
+              decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(200),
+                      bottomRight: Radius.circular(200)),
+                  image: DecorationImage(
+                      image: AssetImage("assets/images/login.png"),
+                      fit: BoxFit.fill)),
+            ),
+            backgroundColor: transparent,
+            elevation: 0,
+          ),
+          body: _isLoading
+              ? Center(
+                  child: CircularProgressIndicator(
+                      color: Theme.of(context).primaryColor))
+              : SingleChildScrollView(
+                  child: Form(
+                    key: _formKey,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        const Text(
+                          "WELCOME BACK",
+                          style: TextStyle(
+                              fontSize: 46,
+                              fontWeight: FontWeight.bold,
+                              fontFamily: 'MyCustomFont',
+                              color: lightestGreen),
+                        ),
+                        const SizedBox(
+                          height: 05,
+                        ),
+                        const Text(
+                          "Login with TungTee account",
+                          style: TextStyle(
+                              fontSize: 14,
+                              fontWeight: FontWeight.w400,
+                              fontFamily: 'MyCustomFont',
+                              color: primaryColor),
+                        ),
+                        const SizedBox(
+                          height: 25,
+                        ),
+                        Container(
+                          alignment: Alignment.center,
+                          width: 360,
+                          child: TextFormField(
+                            controller: emailController,
+                            decoration: textInputDecoration.copyWith(
+                              hintText: 'Email  ',
+                              prefixIcon: const Icon(
+                                Icons.account_circle,
+                                color: green,
+                              ),
+                            ),
+                            onChanged: (val) {
+                              setState(() {
+                                email = val;
+                              });
+                            },
+                            validator: (val) {
+                              return RegExp(
+                                          r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+                                      .hasMatch(val!)
+                                  ? null
+                                  : "Please enter a valid email";
+                            },
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 25,
+                        ),
+
+                        Container(
+                          alignment: Alignment.center,
+                          width: 360,
+                          child: TextFormField(
+                            controller: passwordController,
+                            obscureText: true,
+                            decoration: textInputDecoration.copyWith(
+                              hintText: 'Password',
+                              prefixIcon: const Icon(
+                                Icons.lock,
+                                color: green,
+                              ),
+                            ),
+                            onChanged: (val) {
+                              setState(() {
+                                password = val;
+                              });
+                            },
+                            validator: (val) {
+                              if (val!.length < 6) {
+                                return "Password must be at least 6 characters";
+                              } else {
+                                return null;
+                              }
+                            },
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 7,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            TextButton(
+                                style: TextButton.styleFrom(
+                                  primary: primaryColor,
+                                ),
+                                onPressed: () {
+                                  nextScreen(context, ResetPasswordPage());
+                                },
+                                child: const Text("Forget Password?"))
+                          ],
+                        ),
+                        const SizedBox(
+                          height: 50,
+                        ),
+                        //ElevatedButton.icon(
+                        // onPressed: () {
+                        //   login();
+                        // },
+                        // icon: const Icon(Icons.lock_open),
+                        // label: const Text('Login'),
+                        // ),
+                        ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              backgroundColor: mobileBackgroundColor,
+                              side: const BorderSide(
+                                width: 2.0,
+                                color: purple,
+                              ),
+                              minimumSize: const Size(307, 49),
+                              elevation: 0,
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(30))),
+                          child: const Text(
+                            "Login",
+                            style: TextStyle(color: purple, fontSize: 24),
+                          ),
+                          onPressed: () {
+                            login();
+                          },
+                        ),
+
+                        //TextButton(
+                        // onPressed: () =>
+                        //    Navigator.pushNamed(context, '/register'),
+                        // child: const Text('ยังไม่มีบัญชีหรอ? สมัครใช้งาน'),
+                        // ),
+                        const SizedBox(
+                          height: 15,
+                        ),
+                        Text.rich(TextSpan(
+                          style: const TextStyle(
+                              color: mobileSearchColor,
+                              fontSize: 12,
+                              fontFamily: 'MyCustomFont'),
+                          children: <TextSpan>[
+                            TextSpan(
+                                text: "No account with TungTee? ",
+                                style: const TextStyle(
+                                    color: primaryColor,
+                                    decoration: TextDecoration.none),
+                                recognizer: TapGestureRecognizer()),
+                            TextSpan(
+                                text: "Register",
+                                style: const TextStyle(
+                                    color: lightGreen,
+                                    decoration: TextDecoration.underline,
+                                    fontWeight: FontWeight.bold),
+                                recognizer: TapGestureRecognizer()
+                                  ..onTap = () {
+                                    nextScreen(context, const RegisterPage());
+                                  }),
+                          ],
+                        )),
+                      ],
+                    ),
+                  ),
+                ),
+        ),
+      ),
     );
   }
 
