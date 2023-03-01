32a581c6e79c772b5b1829c2792154a4863a60e1
ppppeace <98012373+ppppeace@users.noreply.github.com>
2023-03-01 15:05:52 +0700

    Update reset_password.dart
    แก้ภาษา

--- lib/services/reset_password.dart
+++ lib/services/reset_password.dart
@@ -18,124 +18,129 @@ class _ResetPasswordPageState extends State<ResetPasswordPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: lightPurple,
-      appBar: AppBar(
-        toolbarHeight: 150,
-        centerTitle: true,
-        flexibleSpace: Container(
-          decoration: const BoxDecoration(
-              image: DecorationImage(
-                  image: AssetImage("assets/images/reset.png"),
-                  fit: BoxFit.scaleDown)),
-        ),
-        backgroundColor: transparent,
-        elevation: 0,
-        automaticallyImplyLeading: false,
-      ),
-      body: SingleChildScrollView(
-        child: Center(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              //  Image.asset("assets/images/reset.png"),
-              const Text("Reset password",
-                  style: TextStyle(
-                      fontSize: 51,
-                      fontWeight: FontWeight.bold,
-                      fontFamily: 'MyCustomFont',
-                      color: lightGreen)),
-              const SizedBox(
-                height: 15,
-              ),
-              const Text("Don’t worry! It happens.",
-                  style: TextStyle(
-                      fontSize: 14,
-                      fontWeight: FontWeight.bold,
-                      fontFamily: 'MyCustomFont',
-                      color: primaryColor)),
-              const Text(
-                  "Plase enter Your E-mail address to reset you password\n",
-                  style: TextStyle(
-                      fontSize: 14,
-                      fontWeight: FontWeight.bold,
-                      fontFamily: 'MyCustomFont',
-                      color: primaryColor)),
-              const SizedBox(
-                height: 35,
-              ),
-              Container(
-                alignment: Alignment.center,
-                width: 360,
-                child: TextField(
-                  controller: _emailController,
-                  decoration: textInputDecoration.copyWith(
-                    hintText: 'Your E-mail',
-                    hintStyle: const TextStyle(),
-                    prefixIcon: const Icon(
-                      Icons.email_outlined,
-                      color: green,
+    return MaterialApp(
+      home: DismissKeyboard(
+        child: Scaffold(
+          backgroundColor: lightPurple,
+          appBar: AppBar(
+            toolbarHeight: 150,
+            centerTitle: true,
+            flexibleSpace: Container(
+              decoration: const BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage("assets/images/reset.png"),
+                      fit: BoxFit.scaleDown)),
+            ),
+            backgroundColor: transparent,
+            elevation: 0,
+            automaticallyImplyLeading: false,
+          ),
+          body: SingleChildScrollView(
+            child: Center(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  //  Image.asset("assets/images/reset.png"),
+                  const Text("Reset password",
+                      style: TextStyle(
+                          fontSize: 51,
+                          fontWeight: FontWeight.bold,
+                          fontFamily: 'MyCustomFont',
+                          color: lightGreen)),
+                  const SizedBox(
+                    height: 15,
+                  ),
+                  const Text("Don’t worry! It happens.",
+                      style: TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.bold,
+                          fontFamily: 'MyCustomFont',
+                          color: primaryColor)),
+                  const Text(
+                      "Plase enter Your E-mail address to reset you password\n",
+                      style: TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.bold,
+                          fontFamily: 'MyCustomFont',
+                          color: primaryColor)),
+                  const SizedBox(
+                    height: 35,
+                  ),
+                  Container(
+                    alignment: Alignment.center,
+                    width: 360,
+                    child: TextField(
+                      controller: _emailController,
+                      decoration: textInputDecoration.copyWith(
+                        hintText: 'Your E-mail',
+                        hintStyle: const TextStyle(),
+                        prefixIcon: const Icon(
+                          Icons.email_outlined,
+                          color: green,
+                        ),
+                      ),
                     ),
                   ),
-                ),
-              ),
-              const SizedBox(
-                height: 45,
-              ),
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                    backgroundColor: primaryColor,
-                    foregroundColor: primaryColor,
-                    side: const BorderSide(
-                      width: 2.0,
-                      color: purple,
+                  const SizedBox(
+                    height: 45,
+                  ),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: primaryColor,
+                        foregroundColor: primaryColor,
+                        side: const BorderSide(
+                          width: 2.0,
+                          color: purple,
+                        ),
+                        minimumSize: const Size(307, 49),
+                        elevation: 0,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30))),
+                    child: const Text(
+                      "Send",
+                      style: TextStyle(color: purple, fontSize: 24),
                     ),
-                    minimumSize: const Size(307, 49),
-                    elevation: 0,
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(30))),
-                child: const Text(
-                  "Send",
-                  style: TextStyle(color: purple, fontSize: 24),
-                ),
-                onPressed: () {
-                  String email = _emailController.text;
-                  _auth.sendPasswordResetEmail(email: email).then((value) {
-                    print('Password reset email sent');
-                    _showResetEmailSentDialog();
-                  }).catchError((error) {
-                    print('Error sending password reset email: $error');
-                    _showErrorDialog();
-                  });
-                },
-              ),
-              const SizedBox(
-                height: 15,
-              ),
-              Text.rich(TextSpan(
-                style: const TextStyle(
-                    color: mobileSearchColor,
-                    fontSize: 12,
-                    fontFamily: 'MyCustomFont'),
-                children: <TextSpan>[
-                  TextSpan(
-                      text: "กลับสู่หน้า",
-                      style: const TextStyle(
-                          color: primaryColor, decoration: TextDecoration.none),
-                      recognizer: TapGestureRecognizer()),
-                  TextSpan(
-                      text: "เข้าสู่ระบบ",
-                      style: const TextStyle(
-                          color: lightGreen,
-                          decoration: TextDecoration.underline,
-                          fontWeight: FontWeight.bold),
-                      recognizer: TapGestureRecognizer()
-                        ..onTap = () {
-                          nextScreen(context, Login());
-                        }),
+                    onPressed: () {
+                      String email = _emailController.text;
+                      _auth.sendPasswordResetEmail(email: email).then((value) {
+                        print('Password reset email sent');
+                        _showResetEmailSentDialog();
+                      }).catchError((error) {
+                        print('Error sending password reset email: $error');
+                        _showErrorDialog();
+                      });
+                    },
+                  ),
+                  const SizedBox(
+                    height: 15,
+                  ),
+                  Text.rich(TextSpan(
+                    style: const TextStyle(
+                        color: mobileSearchColor,
+                        fontSize: 12,
+                        fontFamily: 'MyCustomFont'),
+                    children: <TextSpan>[
+                      TextSpan(
+                          text: "Back to ",
+                          style: const TextStyle(
+                              color: primaryColor,
+                              decoration: TextDecoration.none),
+                          recognizer: TapGestureRecognizer()),
+                      TextSpan(
+                          text: "Login Page",
+                          style: const TextStyle(
+                              color: lightGreen,
+                              decoration: TextDecoration.underline,
+                              fontWeight: FontWeight.bold),
+                          recognizer: TapGestureRecognizer()
+                            ..onTap = () {
+                              nextScreen(context, Login());
+                            }),
+                    ],
+                  )),
                 ],
-              )),
-            ],
+              ),
+            ),
           ),
         ),
       ),
