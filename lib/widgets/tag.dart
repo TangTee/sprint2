aee2c9eba6694400d728084d760d8c062fb27bab
Jirakit Meesapprasert <43947448+hwakky@users.noreply.github.com>
2023-03-02 22:49:58 +0700

    change ui tag

--- lib/widgets/tag.dart
+++ lib/widgets/tag.dart
@@ -26,7 +26,8 @@ showModalBottomSheetC(BuildContext context) {
                     childAspectRatio: 2 / 2,
                     crossAxisSpacing: 30,
                     mainAxisSpacing: 20),
-                padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
                 itemCount: (snapshot.data! as dynamic).docs.length,
                 itemBuilder: (context, index) {
                   final DocumentSnapshot documentSnapshot =
@@ -93,292 +94,6 @@ showModalBottomSheetC(BuildContext context) {
   return value;
 }
 
-// showModalBottomSheetT(BuildContext context, categoryId, value) {
-//   final CollectionReference _tags =
-//       FirebaseFirestore.instance.collection('tags');
-
-//   showModalBottomSheet(
-//     useRootNavigator: true,
-//     context: context,
-//     builder: (BuildContext context) {
-//       return Container(
-//         height: MediaQuery.of(context).size.height * 0.5,
-//         width: MediaQuery.of(context).size.width * 0.5,
-//         child: StreamBuilder(
-//           stream: _tags.where("categoryId", isEqualTo: categoryId).snapshots(),
-//           builder: ((context, AsyncSnapshot<QuerySnapshot> snapshot) {
-//             if (snapshot.hasData) {
-//               return Padding(
-//                 padding: const EdgeInsets.all(20),
-//                 child: SizedBox(
-//                   child: Expanded(
-//                     child: ListView.builder(
-//                       padding: new EdgeInsets.only(top: 10.0),
-//                       physics: const AlwaysScrollableScrollPhysics(),
-//                       scrollDirection: Axis.horizontal,
-//                       shrinkWrap: true,
-//                       itemCount: (snapshot.data! as dynamic).docs.length,
-//                       itemBuilder: (context, index) {
-//                         final DocumentSnapshot documentSnapshot =
-//                             snapshot.data!.docs[index];
-
-//                         var Mytext = new Map();
-//                         Mytext['tag'] = documentSnapshot['tag'];
-//                         Mytext['tagColor'] = documentSnapshot['tagColor'];
-
-//                         return Wrap(direction: Axis.horizontal, children: <
-//                             Widget>[
-//                           Container(
-//                               width: MediaQuery.of(context).size.width * 0.3,
-//                               height: MediaQuery.of(context).size.height * 0.1,
-//                               child: Column(children: [
-//                                 SizedBox(
-//                                   child: OutlinedButton(
-//                                     onPressed: () {
-//                                       value['_tag2'] = Mytext['tag'].toString();
-//                                       value['_tag2Color'] =
-//                                           Mytext['tagColor'].toString();
-//                                       Navigator.of(context).pop();
-//                                       Navigator.of(context).pop();
-//                                     },
-//                                     child: Text(
-//                                       Mytext['tag'],
-//                                       style: const TextStyle(
-//                                           color: mobileSearchColor,
-//                                           fontSize: 14),
-//                                     ),
-//                                     style: OutlinedButton.styleFrom(
-//                                         shape: RoundedRectangleBorder(
-//                                             borderRadius:
-//                                                 BorderRadius.circular(30)),
-//                                         side: BorderSide(
-//                                             color: HexColor(
-//                                               Mytext['tagColor'],
-//                                             ),
-//                                             width: 1.5)),
-//                                   ),
-//                                 ),
-//                               ])),
-//                         ]);
-//                       },
-//                     ),
-//                   ),
-//                 ),
-//               );
-//             }
-//             return Center(
-//               child: Column(
-//                 crossAxisAlignment: CrossAxisAlignment.center,
-//                 children: const <Widget>[
-//                   SizedBox(
-//                     height: 30.0,
-//                     width: 30.0,
-//                     child: CircularProgressIndicator(),
-//                   ),
-//                 ],
-//               ),
-//             );
-//           }),
-//         ),
-//       );
-//     },
-//   );
-//   return value;
-// }
-
-// showModalBottomSheetT(BuildContext context, categoryId, value) {
-//   final CollectionReference _tags =
-//       FirebaseFirestore.instance.collection('tags');
-
-//   showModalBottomSheet(
-//     useRootNavigator: true,
-//     context: context,
-//     builder: (BuildContext context) {
-//       return Scaffold(
-//           body: Column(
-//         children: [
-//           Flexible(
-//             child: StreamBuilder(
-//               stream:
-//                   _tags.where("categoryId", isEqualTo: categoryId).snapshots(),
-//               builder: ((context, AsyncSnapshot<QuerySnapshot> snapshot) {
-//                 if (snapshot.hasData) {
-//                   return ListView.builder(
-//                     padding: EdgeInsets.only(top: 10.0),
-//                     itemCount: (snapshot.data! as dynamic).docs.length,
-//                     itemBuilder: (context, index) {
-//                       final DocumentSnapshot documentSnapshot =
-//                           snapshot.data!.docs[index];
-
-//                       var Mytext = new Map();
-//                       Mytext['tag'] = documentSnapshot['tag'];
-//                       Mytext['tagColor'] = documentSnapshot['tagColor'];
-
-//                       return Wrap(children: [
-//                         SizedBox(
-//                             // width: MediaQuery.of(context).size.width * 0.3,
-//                             // height: MediaQuery.of(context).size.height * 0.1,
-//                             width: 100,
-//                             height: 50,
-//                             child: Column(children: [
-//                               SizedBox(
-//                                 child: OutlinedButton(
-//                                   onPressed: () {
-//                                     value['_tag2'] = Mytext['tag'].toString();
-//                                     value['_tag2Color'] =
-//                                         Mytext['tagColor'].toString();
-//                                     Navigator.of(context).pop();
-//                                     Navigator.of(context).pop();
-//                                   },
-//                                   child: Text(
-//                                     Mytext['tag'],
-//                                     style: const TextStyle(
-//                                         color: mobileSearchColor, fontSize: 14),
-//                                   ),
-//                                   style: OutlinedButton.styleFrom(
-//                                       shape: RoundedRectangleBorder(
-//                                           borderRadius:
-//                                               BorderRadius.circular(30)),
-//                                       side: BorderSide(
-//                                           color: HexColor(
-//                                             Mytext['tagColor'],
-//                                           ),
-//                                           width: 1.5)),
-//                                 ),
-//                               ),
-//                             ])),
-//                       ]);
-//                     },
-//                   );
-//                 }
-//                 return Center(
-//                   child: Column(
-//                     crossAxisAlignment: CrossAxisAlignment.center,
-//                     children: const <Widget>[
-//                       SizedBox(
-//                         height: 30.0,
-//                         width: 30.0,
-//                         child: CircularProgressIndicator(),
-//                       ),
-//                     ],
-//                   ),
-//                 );
-//               }),
-//             ),
-//           )
-//         ],
-//       ));
-//     },
-//   );
-//   return value;
-// }
-
-// showModalBottomSheetT(BuildContext context, categoryId, value) {
-//   final CollectionReference _tags =
-//       FirebaseFirestore.instance.collection('tags');
-
-//   showModalBottomSheet(
-//     useRootNavigator: true,
-//     context: context,
-//     builder: (BuildContext context) {
-//       return SingleChildScrollView(
-//         child: Column(
-//           children: [
-//             SizedBox(
-//               width: MediaQuery.of(context).size.width * 0.9,
-//               height: MediaQuery.of(context).size.height * 0.9,
-//               child: StreamBuilder(
-//                 stream: _tags
-//                     .where("categoryId", isEqualTo: categoryId)
-//                     .snapshots(),
-//                 builder: ((context, AsyncSnapshot<QuerySnapshot> snapshot) {
-//                   if (snapshot.hasData) {
-//                     return Padding(
-//                       padding: const EdgeInsets.all(20),
-//                       child: SizedBox(
-//                         child: Expanded(
-//                           child: ListView.builder(
-//                             padding: new EdgeInsets.only(top: 10.0),
-//                             // physics: const AlwaysScrollableScrollPhysics(),
-//                             // scrollDirection: Axis.horizontal,
-//                             shrinkWrap: true,
-//                             itemCount: (snapshot.data! as dynamic).docs.length,
-//                             itemBuilder: (context, index) {
-//                               final DocumentSnapshot documentSnapshot =
-//                                   snapshot.data!.docs[index];
-
-//                               var Mytext = new Map();
-//                               Mytext['tag'] = documentSnapshot['tag'];
-//                               Mytext['tagColor'] = documentSnapshot['tagColor'];
-
-//                               // return Wrap(direction: Axis.vertical, children: <
-//                               //     Widget>[
-//                               return Wrap(children: [
-//                                 Container(
-//                                     width:
-//                                         MediaQuery.of(context).size.width * 0.3,
-//                                     height: MediaQuery.of(context).size.height *
-//                                         0.1,
-//                                     child: Column(children: [
-//                                       SizedBox(
-//                                         child: OutlinedButton(
-//                                           onPressed: () {
-//                                             value['_tag2'] =
-//                                                 Mytext['tag'].toString();
-//                                             value['_tag2Color'] =
-//                                                 Mytext['tagColor'].toString();
-//                                             Navigator.of(context).pop();
-//                                             Navigator.of(context).pop();
-//                                           },
-//                                           child: Text(
-//                                             Mytext['tag'],
-//                                             style: const TextStyle(
-//                                                 color: mobileSearchColor,
-//                                                 fontSize: 14),
-//                                           ),
-//                                           style: OutlinedButton.styleFrom(
-//                                               shape: RoundedRectangleBorder(
-//                                                   borderRadius:
-//                                                       BorderRadius.circular(
-//                                                           30)),
-//                                               side: BorderSide(
-//                                                   color: HexColor(
-//                                                     Mytext['tagColor'],
-//                                                   ),
-//                                                   width: 1.5)),
-//                                         ),
-//                                       ),
-//                                     ])),
-//                               ]);
-//                             },
-//                           ),
-//                         ),
-//                       ),
-//                     );
-//                   }
-//                   return Center(
-//                     child: Column(
-//                       crossAxisAlignment: CrossAxisAlignment.center,
-//                       children: const <Widget>[
-//                         SizedBox(
-//                           height: 30.0,
-//                           width: 30.0,
-//                           child: CircularProgressIndicator(),
-//                         ),
-//                       ],
-//                     ),
-//                   );
-//                 }),
-//               ),
-//             ),
-//           ],
-//         ),
-//       );
-//     },
-//   );
-//   return value;
-// }
-
 showModalBottomSheetT(BuildContext context, categoryId, value) {
   final CollectionReference tags =
       FirebaseFirestore.instance.collection('tags');
@@ -400,10 +115,10 @@ showModalBottomSheetT(BuildContext context, categoryId, value) {
                   child: GridView.builder(
                       gridDelegate:
                           const SliverGridDelegateWithMaxCrossAxisExtent(
-                              maxCrossAxisExtent: 100,
+                              maxCrossAxisExtent: 145,
                               childAspectRatio: 3 / 2,
-                              crossAxisSpacing: 30,
-                              mainAxisSpacing: 20),
+                              crossAxisSpacing: 20,
+                              mainAxisSpacing: 5),
                       itemCount: (snapshot.data! as dynamic).docs.length,
                       itemBuilder: (BuildContext ctx, index) {
                         final DocumentSnapshot documentSnapshot =
@@ -413,7 +128,7 @@ showModalBottomSheetT(BuildContext context, categoryId, value) {
                         Mytext['tag'] = documentSnapshot['tag'];
                         Mytext['tagColor'] = documentSnapshot['tagColor'];
                         return SizedBox(
-                            width: MediaQuery.of(context).size.width * 0.3,
+                            width: MediaQuery.of(context).size.width * 0.5,
                             height: MediaQuery.of(context).size.height * 0.1,
                             child: Column(children: [
                               SizedBox(
