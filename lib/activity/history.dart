cec9c3ba937d010d71efe2e74e10015a9d78fae1
PichsineeA <98012317+PichsineeA@users.noreply.github.com>
2023-03-02 22:45:05 +0700

    Update history.dart
    fix bug

--- lib/activity/history.dart
+++ lib/activity/history.dart
@@ -5,59 +5,63 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 import '../widgets/PostCard.dart';
 
-class HistoryPage extends StatelessWidget {
-  const HistoryPage({Key? key, required}) : super(key: key);
+class HistoryPage extends StatefulWidget {
+  const HistoryPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return  Scaffold(
-      backgroundColor: mobileBackgroundColor,
-      body: WaitingCard(),
-    );
-  }
+  _HistoryPageState createState() => _HistoryPageState();
 }
 
-class WaitingCard extends StatelessWidget {
+class _HistoryPageState extends State<HistoryPage> {
   final _post = FirebaseFirestore.instance.collection('post');
 
-   WaitingCard({super.key});
+  // WaitingCard({super.key});
+  void setState(VoidCallback fn) {
+    if (mounted) {
+      super.setState(fn);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: mobileBackgroundColor,
-      child: StreamBuilder<QuerySnapshot>(
-        stream: FirebaseFirestore.instance
-            .collection('post')
-            .where('history',
-                arrayContains: FirebaseAuth.instance.currentUser!.uid)
-            .where('open', isEqualTo: false)
-            .orderBy('timeStamp', descending: true)
-            .snapshots(),
-        builder: (context, AsyncSnapshot<QuerySnapshot> streamSnapshot) {
-          if (streamSnapshot.hasData) {
-            return ListView.builder(
-                itemCount: streamSnapshot.data!.docs.length,
-                itemBuilder: (context, index) => Container(
-                      child: CardWidget(
-                          snap: (streamSnapshot.data! as dynamic).docs[index]),
-                    ));
-          }
-          return Container(
-            child: Center(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: const <Widget>[
-                  SizedBox(
-                    height: 30.0,
-                    width: 30.0,
-                    child: CircularProgressIndicator(),
-                  ),
-                ],
+    return Scaffold(
+      backgroundColor: mobileBackgroundColor,
+      body: Container(
+        color: mobileBackgroundColor,
+        child: StreamBuilder<QuerySnapshot>(
+          stream: FirebaseFirestore.instance
+              .collection('post')
+              .where('history',
+                  arrayContains: FirebaseAuth.instance.currentUser!.uid)
+              .where('open', isEqualTo: false)
+              .orderBy('timeStamp', descending: true)
+              .snapshots(),
+          builder: (context, AsyncSnapshot<QuerySnapshot> streamSnapshot) {
+            if (streamSnapshot.hasData) {
+              return ListView.builder(
+                  itemCount: streamSnapshot.data!.docs.length,
+                  itemBuilder: (context, index) => Container(
+                        child: CardWidget(
+                            snap:
+                                (streamSnapshot.data! as dynamic).docs[index]),
+                      ));
+            }
+            return Container(
+              child: Center(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: const <Widget>[
+                    SizedBox(
+                      height: 30.0,
+                      width: 30.0,
+                      child: CircularProgressIndicator(),
+                    ),
+                  ],
+                ),
               ),
-            ),
-          );
-        },
+            );
+          },
+        ),
       ),
     );
   }
